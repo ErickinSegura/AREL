8caca7701aa19c60c7ce87ab73708365bb37370c
ErickinSegura <108953788+ErickinSegura@users.noreply.github.com>
2025-03-06 11:32:55 -0600

    Card fix

--- src/main/frontend/src/components/ui/Card.jsx
+++ src/main/frontend/src/components/ui/Card.jsx
@@ -18,7 +18,9 @@ const CardTitle = ({ className, ...props }) => {
         <h3
             className={`text-2xl font-semibold leading-none tracking-tight ${className}`}
             {...props}
-        />
+        >
+            {props.children}
+        </h3>
     );
 };
 
