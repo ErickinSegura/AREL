dbeb44f0e1096d0360bf51dd52af8c2c18aff2db
ErickinSegura <108953788+ErickinSegura@users.noreply.github.com>
2025-03-28 21:53:23 -0600

    Ahora si login back hecho

--- src/main/java/com/springboot/MyTodoList/model/User.java
+++ src/main/java/com/springboot/MyTodoList/model/User.java
@@ -1,5 +1,8 @@
 package com.springboot.MyTodoList.model;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -20,6 +23,7 @@ import java.util.Set;
 @AllArgsConstructor
 @Builder
 @ToString
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class User implements UserDetails {
 
     @Id
