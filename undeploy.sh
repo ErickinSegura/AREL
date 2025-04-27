f7350bcfbd6a1d6f0e874880e183b69fb6a0a93e
Rodrigo López Murguía <A01742780@tec.mx>
2025-04-27 14:05:38 -0600

    Update undeploy.sh

--- undeploy.sh
+++ undeploy.sh
@@ -2,3 +2,4 @@ echo delete frontend deployment and service...
 kubectl -n mtdrworkshop delete deployment todolistapp-springboot-deployment
 kubectl -n mtdrworkshop delete deployment todolistapp-python-deployment
 kubectl -n mtdrworkshop delete service todolistapp-springboot-service
+kubectl -n mtdrworkshop delete service todolistapp-python-service
