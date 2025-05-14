7e7d94903e6127e6aa177686ccbdc7bbf2701b36
ErickinSegura <108953788+ErickinSegura@users.noreply.github.com>
2025-05-14 12:54:56 -0600

    cors antiguo

--- src/main/java/com/springboot/MyTodoList/security/WebSecurityConfiguration.java
+++ src/main/java/com/springboot/MyTodoList/security/WebSecurityConfiguration.java
@@ -1,4 +1,4 @@
-package com.springboot.MyTodoList.security;
+/*package com.springboot.MyTodoList.security;
 
 import com.springboot.MyTodoList.security.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
@@ -38,6 +38,46 @@ public class WebSecurityConfiguration {
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}*/
+
+package com.springboot.MyTodoList.security;
+
+import com.springboot.MyTodoList.security.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfiguration {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .antMatchers("/", "/index.html", "/static/**",
+                                        "/*.js", "/*.css", "/*.ico", "/*.json", "/*.png").permitAll()
+                                .antMatchers("/auth/login", "/auth/register").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
